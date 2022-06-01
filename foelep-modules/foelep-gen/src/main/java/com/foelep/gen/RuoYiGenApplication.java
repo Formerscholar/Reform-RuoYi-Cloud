d17819e96334673831c4434d71937ad9854019b8
qq231931 <coderzc2020@gmail.com>
2022-06-01 10:57:12 +0800

    fix

--- foelep-modules/foelep-gen/src/main/java/com/foelep/gen/RuoYiGenApplication.java
+++ foelep-modules/foelep-gen/src/main/java/com/foelep/gen/RuoYiGenApplication.java
@@ -1,10 +1,10 @@
 package com.foelep.gen;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.foelep.common.security.annotation.EnableCustomConfig;
 import com.foelep.common.security.annotation.EnableRyFeignClients;
 import com.foelep.common.swagger.annotation.EnableCustomSwagger2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 代码生成
@@ -20,15 +20,5 @@ public class RuoYiGenApplication
     public static void main(String[] args)
     {
         SpringApplication.run(RuoYiGenApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  代码生成模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
     }
 }

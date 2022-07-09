a625e02d2eab636829fecc4974a35ff4fcdbed45
chad <2319315514@qq.com>
2022-07-09 21:33:07 +0800

    修复工作流模型无法保存问题(XSS过滤规则修改)

--- foelep-gateway/src/main/java/com/foelep/gateway/filter/XssFilter.java
+++ foelep-gateway/src/main/java/com/foelep/gateway/filter/XssFilter.java
@@ -1,6 +1,8 @@
 package com.foelep.gateway.filter;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -32,45 +34,41 @@ import reactor.core.publisher.Mono;
  */
 @Component
 @ConditionalOnProperty(value = "security.xss.enabled", havingValue = "true")
-public class XssFilter implements GlobalFilter, Ordered
-{
+public class XssFilter implements GlobalFilter, Ordered {
     // 跨站脚本的 xss 配置，nacos自行添加
     @Autowired
     private XssProperties xss;
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
-    {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         // GET DELETE 不过滤
         HttpMethod method = request.getMethod();
-        if (method == null || method.matches("GET") || method.matches("DELETE"))
-        {
+        if (method == null || method.matches("GET") || method.matches("DELETE")) {
             return chain.filter(exchange);
         }
         // 非json类型，不过滤
-        if (!isJsonRequest(exchange))
-        {
+        if (!isJsonRequest(exchange)) {
             return chain.filter(exchange);
         }
         // excludeUrls 不过滤
         String url = request.getURI().getPath();
-        if (StringUtils.matches(url, xss.getExcludeUrls()))
-        {
-            return chain.filter(exchange);
+        List<String> list = xss.getExcludeUrls();
+        for (String sexcludeUrls : list) {
+            if (StringUtils.isMatch(sexcludeUrls, url)) {
+                return chain.filter(exchange);
+            }
         }
+
         ServerHttpRequestDecorator httpRequestDecorator = requestDecorator(exchange);
         return chain.filter(exchange.mutate().request(httpRequestDecorator).build());
 
     }
 
-    private ServerHttpRequestDecorator requestDecorator(ServerWebExchange exchange)
-    {
-        ServerHttpRequestDecorator serverHttpRequestDecorator = new ServerHttpRequestDecorator(exchange.getRequest())
-        {
+    private ServerHttpRequestDecorator requestDecorator(ServerWebExchange exchange) {
+        ServerHttpRequestDecorator serverHttpRequestDecorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
             @Override
-            public Flux<DataBuffer> getBody()
-            {
+            public Flux<DataBuffer> getBody() {
                 Flux<DataBuffer> body = super.getBody();
                 return body.buffer().map(dataBuffers -> {
                     DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
@@ -91,8 +89,7 @@ public class XssFilter implements GlobalFilter, Ordered
             }
 
             @Override
-            public HttpHeaders getHeaders()
-            {
+            public HttpHeaders getHeaders() {
                 HttpHeaders httpHeaders = new HttpHeaders();
                 httpHeaders.putAll(super.getHeaders());
                 // 由于修改了请求体的body，导致content-length长度不确定，因此需要删除原先的content-length
@@ -107,18 +104,16 @@ public class XssFilter implements GlobalFilter, Ordered
 
     /**
      * 是否是Json请求
-     * 
+     *
      * @param exchange HTTP请求
      */
-    public boolean isJsonRequest(ServerWebExchange exchange)
-    {
+    public boolean isJsonRequest(ServerWebExchange exchange) {
         String header = exchange.getRequest().getHeaders().getFirst(HttpHeaders.CONTENT_TYPE);
         return StringUtils.startsWithIgnoreCase(header, MediaType.APPLICATION_JSON_VALUE);
     }
 
     @Override
-    public int getOrder()
-    {
+    public int getOrder() {
         return -100;
     }
 }
